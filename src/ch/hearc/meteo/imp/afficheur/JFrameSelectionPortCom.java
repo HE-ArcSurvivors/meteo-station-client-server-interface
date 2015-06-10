3c999659d964ff1852fe14504c591916a7e2068d
Margaux Divernois <margaux.divernois@gmail.com>
2015-06-10 16:10:40 +0200

    Port différent

--- src/ch/hearc/meteo/imp/afficheur/JFrameSelectionPortCom.java
+++ src/ch/hearc/meteo/imp/afficheur/JFrameSelectionPortCom.java
@@ -206,7 +206,7 @@ public class JFrameSelectionPortCom extends JFrame
 
 	private void launchSimulation() throws MeteoServiceException
 		{
-		launchStation("SIMULATION");
+		launchStation("SIMULATEUR");
 		}
 
 	/*------------------------------------------------------------------*\
